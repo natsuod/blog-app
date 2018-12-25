c112fd0957d16d671d22ff1f7eec1bf906efcb8a
natsuod <odanatsumi@OdanoMac-mini.local>
2018-12-25 14:52:03 +0900

    Edit-controller.rb-to-change-order

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,7 +1,7 @@
 class PostsController < ApplicationController
   before_action :move_to_index, except: :index
   def index
-    @posts = Post.all
+    @posts = Post.order("created_at DESC")
   end
 
   def new
