05823b50f7237040cc035527c9a573727b212428
natsuod <odanatsumi@OdanoMac-mini.local>
2018-12-25 11:00:26 +0900

    add-edit-action-to-routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -6,4 +6,5 @@ Rails.application.routes.draw do
   post 'posts' => 'posts#create'
   get 'posts/:id' => 'posts#show'
   delete 'posts/:id' => 'posts#destroy'
+  get 'posts/:id/edit' => 'posts#edit'
 end
